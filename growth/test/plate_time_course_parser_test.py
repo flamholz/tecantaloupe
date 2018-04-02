f1b48cbf6dc96f5ee26e2bbfbb4f33fed0b11fcb
Avi Flamholz <flamholz@gmail.com>
2018-04-02 16:42:36 -0700

    More expansive testing of parser.

--- growth/test/plate_time_course_parser_test.py
+++ growth/test/plate_time_course_parser_test.py
@@ -7,6 +7,7 @@ import pandas
 import pylab
 
 from growth.plate_time_course_parser import SavageLabM1000Excel
+from growth.plate_spec import PlateSpec
 
 
 class PlateTimecourseParserTest(unittest.TestCase):
@@ -19,6 +20,15 @@ class PlateTimecourseParserTest(unittest.TestCase):
 
         df = timecourse._well_df
 
+        expected_wells = set(PlateSpec.ALL_WELL_NAMES)
+        actual_wells = set(df['abs600'].columns)
+        self.assertTrue('time_s' in actual_wells)
+        self.assertTrue('temp_C' in actual_wells)
+        actual_wells.discard('time_s')
+        actual_wells.discard('temp_C')
+
+        self.assertEqual(expected_wells, actual_wells)
+
     def testMultipleParse(self):
         parser = SavageLabM1000Excel()
         timecourse = parser.ParseFromFilename(
@@ -26,6 +36,14 @@ class PlateTimecourseParserTest(unittest.TestCase):
 
         df = timecourse._well_df
 
+        expected_wells = set(PlateSpec.ALL_WELL_NAMES)
+        actual_wells = set(df['GFP'].columns)
+        self.assertTrue('time_s' in actual_wells)
+        self.assertTrue('temp_C' in actual_wells)
+        actual_wells.discard('time_s')
+        actual_wells.discard('temp_C')
+
+        self.assertEqual(expected_wells, actual_wells)
 
 if __name__ == '__main__':
     unittest.main()
